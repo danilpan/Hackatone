2d1c118b5d1f363536ebebd74ed583908bbe3739
danilpan <Aigerim3006>
2023-07-28 13:12:05 +0600

    fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/madxiii/hackatone/app"
@@ -12,6 +11,4 @@ func main() {
 	if err != nil {
 		log.Fatalf("application err: %v\n", err)
 	}
-
-	fmt.Println("Cfg", a)
 }
